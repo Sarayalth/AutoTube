f6f15dccde214d18297f52b402d976056abc15e6
Saray <sarayalth@gmail.com>
2020-03-08 12:13:17 +0100

    fixed button styling and rewrited extension to use videoplayer loop option instead of a custom code

--- ytauto.js
+++ ytauto.js
@@ -2,56 +2,43 @@ linkStart();
 
 function linkStart() {
 	if (window.location.pathname == "/watch") {
-    var div = document.createElement('div');
+		var div = document.createElement('div');
+		var player = document.getElementsByClassName("video-stream html5-main-video");
+		var seekBar = document.getElementsByClassName("ytp-play-progress ytp-swatch-background-color");
+		var playButton = document.getElementsByClassName("ytp-play-button ytp-button");
+		
+		div.className = 'ytp-menuitem';
+		div.setAttribute("role", "menuitemcheckbox");
+		div.setAttribute("aria-checked", "false");
+		div.tabIndex = '0';
+		div.id = "AC";
+		div.onclick = function (){
+			if (document.getElementById("AC").getAttribute("aria-checked")=="true") {
+				document.getElementById("AC").setAttribute("aria-checked", false);
+				player[0].loop = false;
+			}
+		
+			else if (document.getElementById("AC").getAttribute("aria-checked")=="false") {
+				document.getElementById("AC").setAttribute("aria-checked", true);
+				if (seekBar[0].style.transform == "scaleX(1)")
+				{
+					player[0].play();
+				}
+				else if (seekBar[0].style.transform != "scaleX(1)" && playButton[0].title == "Replay")
+				{
+					player[0].currentTime = 0;
+					player[0].play();
+				}
+				player[0].loop = true;
+			}
+		};
 
-    div.className = 'ytp-menuitem';
-	div.setAttribute("role", "menuitemcheckbox");
-	div.setAttribute("aria-checked", "false");
-	div.tabIndex = '0';
-	div.id = "AC";
-	div.onclick = function (){
-	if (document.getElementById("AC").getAttribute("aria-checked")=="true") {
-		document.getElementById("AC").setAttribute("aria-checked", false);
-		shitHappens();
-	}
-	
-	else if (document.getElementById("AC").getAttribute("aria-checked")=="false") {
-		document.getElementById("AC").setAttribute("aria-checked", true);
-		shitHappens();
-	}
-};
+		div.innerHTML = '<div class="ytp-menuitem-icon"></div><div class="ytp-menuitem-label">AutoReplay</div><div class="ytp-menuitem-content"><div class="ytp-menuitem-toggle-checkbox"></div></div>';
 
-    div.innerHTML = '<div class="ytp-menuitem-label">AutoReplay</div><div class="ytp-menuitem-content"><div class="ytp-menuitem-toggle-checkbox"></div></div>';
-
-    document.getElementsByClassName('ytp-panel-menu')[0].appendChild(div);
-	
-	shitHappens();
+		document.getElementsByClassName('ytp-panel-menu')[0].appendChild(div);
+		
 	}
 	else {
-	setTimeout(linkStart, 5000);
-	}
-}
-
-function shitHappens() {
- if (window.location.pathname == "/watch") {
-	if (document.getElementById("AC").getAttribute("aria-checked")=="true") {
-	var nowtime= document.getElementsByClassName("ytp-progress-bar")[0].getAttribute("aria-valuenow");
-	var maxtime= document.getElementsByClassName("ytp-progress-bar")[0].getAttribute("aria-valuemax");
-	var input = document.getElementsByClassName("ytp-play-button ytp-button");
-
-	
-	if (nowtime == maxtime)
-		{
-			console.log("Trying to press Replay button");
-			input[0].click();
-		} 
-	setTimeout(shitHappens, 5000);
-	}
-	else {
-		setTimeout(shitHappens, 5000);
-	}
- }
- else {
-	setTimeout(shitHappens, 5000);
+		setTimeout(linkStart, 5000);
 	}
 }
